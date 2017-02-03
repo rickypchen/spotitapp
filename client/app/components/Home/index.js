1f6116eaca05b1a94e0f3ff0e289ca1933435808
rickypchen <richard.polin.chen@gmail.com>
2017-02-03 14:04:46 -0500

    added buttons, login and signup

--- client/app/components/Home/index.js
+++ client/app/components/Home/index.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
-import { default as Sidebar } from "../Sidebar"
+import cssModules from "react-css-modules"
 import { default as style } from "./style.css"
 
+import { default as Signup } from "../Signup"
+import { default as Login } from "../Login"
+
 export class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formState: "signup"
+    }
+  }
+
   render() {
     return (
-      <div>
-        <Sidebar />
-        <div className={style.chatWrapper}>
-          Home component
-        </div>
+      <div className={style.leader}>
+        <h1 className={style.title}>Phoenix Chat</h1>
+        {this.state.formState === "signup" ? <Signup /> : null}
+        {this.state.formState === "login" ? <Login /> : null}
+        <img
+          role="presentation"
+          className={style.circles}
+          src="https://s3.amazonaws.com/learnphoenix-static-assets/images/circles-full.png" />
       </div>
     )
   }
 }
 
-export default Home
+export default cssModules(Home, style)
