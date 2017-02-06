d3f37a606f7df2fb9a759b15ff2ed0caa2195394
rickypchen <richard.polin.chen@gmail.com>
2017-02-06 16:43:38 -0500

    added marker and ability to change popup

--- client/app/components/MapView/index.js
+++ client/app/components/MapView/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react"
 import cssModules from "react-css-modules"
 import style from "./style.css"
-import { Map, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import ReactDOM from 'react-dom'
+import { default as Button } from "../Button"
 
 class MapView extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      position: [51.0, -0.09]
+      position: "",
+      popupValue: "You are here!"
     }
+    this.submit = this.submit.bind(this)
   }
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -21,16 +24,43 @@ class MapView extends Component {
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     )
   }
+  submit(e){
+    e.preventDefault()
+    let popupValue = document.getElementById("discovery-value").value
+    this.setState({popupValue})
+    document.getElementById("discovery-value").value = ""
+  }
   render() {
     return (
       <div>
+        <div className={style.form}>
+          <div className={style.inputGroup}>
+            <input
+              placeholder="What do you spot?"
+              className={style.input}
+              type="text"
+              id="discovery-value" />
+          </div>
+          <Button
+            onClick={this.submit}
+            style={{ width: "100%" }}
+            type="primary">
+            SpotIt
+          </Button>
+        </div>
         <Map
-          style={{height: "100vh"}}
+          style={{height: "100vh", zIndex: 1}}
           center={this.state.position}
-          zoom={10}>
+          zoom={15}>
           <TileLayer
             url="https://api.mapbox.com/styles/v1/rickypchen/cixqrjgz300002rn234h1pl6p/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicmlja3lwY2hlbiIsImEiOiJDb053LVV3In0.6qUdY5kyjHAerYaO7CB76A"
             attribution="<attribution>" />
+          <Marker
+            position={this.state.position}>
+            <Popup>
+              <span id="current-location">{this.state.popupValue}</span>
+            </Popup>
+          </Marker>
         </Map>
       </div>
     )
